fa792672d06fc19146eaf39a02ea61f101038e0b
Aleksey Belchenkov <u608110@gmail.com>
2019-03-26 05:47:17 +0300

    Config App

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin_email' => 'belchenkov.leha@mail.ru',
+    'shop_name' => 'Магазин ishop',
+    'pagination' => 3,
+    'smtp_login' => '',
+    'smtp_password' => '',
+];
---
+++ public/index.php
@@ -0,0 +1,1 @@
+<h1>Front Controller</h1>
