435e6a54ecefa69bf8983a3de69418756a53662b
Aleksey Belchenkov <u608110@gmail.com>
2019-03-31 10:11:43 +0300

    Class Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,12 +3,16 @@
 namespace app\controllers;
 
 use ishop\App;
+use ishop\Cache;
 
 class MainController extends AppController
 {
     public function indexAction()
     {
         $posts = \R::findAll('test');
+
+        $cache = Cache::instance();
+        $cache->delete('test');
 
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание ...', 'Ключевые слова ...');
         $this->set(compact('posts'));
