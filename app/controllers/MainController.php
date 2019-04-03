6cd5fd7cf74fe882b492f6da17fc679a68a626aa
Aleksey Belchenkov <u608110@gmail.com>
2019-04-03 05:51:14 +0300

    Output Favorite Products

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,8 +9,9 @@ class MainController extends AppController
     public function indexAction()
     {
         $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
 
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание ...', 'Ключевые слова ...');
-        $this->set(compact('brands'));
+        $this->set(compact('brands', 'hits'));
     }
 }
