fb829b36d84b3f53f4d2b008d8485f18fb440f54
Aleksey Belchenkov <u608110@gmail.com>
2019-04-03 05:27:39 +0300

    Output Brands

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,9 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = \R::find('brand', 'LIMIT 3');
+
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание ...', 'Ключевые слова ...');
+        $this->set(compact('brands'));
     }
 }
