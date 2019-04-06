715ad58a061a9bba5f9a08ccab574325895a76f0
Aleksey Belchenkov <u608110@gmail.com>
2019-04-06 07:56:06 +0300

    Get All Category, save to cache

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -6,6 +6,7 @@ use app\models\AppModel;
 use app\widgets\currency\Currency;
 use ishop\App;
 use ishop\base\Controller;
+use ishop\Cache;
 
 class AppController extends Controller
 {
@@ -15,5 +16,19 @@ class AppController extends Controller
         new AppModel();
         App::$app->setProperty('currencies', Currency::getCurrencies());
         App::$app->setProperty('currency', Currency::getCurrency(App::$app->getProperty('currencies')));
+        App::$app->setProperty('cats', self::cacheCategory());
+    }
+
+    public static function cacheCategory()
+    {
+        $cache = Cache::instance();
+        $cats = $cache->get('cats');
+
+        if (!$cats) {
+            $cats = \R::getAssoc("SELECT * FROM category");
+            $cache->set('cats', $cats);
+        }
+
+        return $cats;
     }
 }
