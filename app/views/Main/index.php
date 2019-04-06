4f5afd04d0752e162bf960efbcafa62a89cf0781
Aleksey Belchenkov <u608110@gmail.com>
2019-04-06 06:24:17 +0300

    Price product dependencies from currency

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -42,6 +42,7 @@
 
 <!--product-starts-->
 <?php if ($hits) : ?>
+<?php $curr = \ishop\App::$app->getProperty('currency'); ?>
     <div class="product">
         <div class="container">
             <div class="product-top">
@@ -63,9 +64,15 @@
                                     <p>Explore Now</p>
                                     <h4>
                                         <a class="add-to-cart-link" href="cart/add?id=<?= $hit->id; ?>"><i></i></a>
-                                        <span class=" item_price">$ <?= $hit->price; ?></span>
+                                        <span class=" item_price">
+                                            <?=$curr['symbol_left'];?> <?= $hit->price * $curr['value']; ?> <?=$curr['symbol_right'];?>
+                                        </span>
                                         <?php if ($hit->old_price) : ?>
-                                            <small><del><?= $hit->old_price; ?></del></small>
+                                            <small><del>
+                                                    <?=$curr['symbol_left'];?>
+                                                    <?= $hit->old_price * $curr['value']; ?>
+                                                    <?=$curr['symbol_right'];?>
+                                                </del></small>
                                         <?php endif; ?>
                                     </h4>
                                 </div>
