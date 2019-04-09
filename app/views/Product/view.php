85033a3ffe25d9f0c1a11864b4d10e3cb7e682a2
Aleksey Belchenkov <u608110@gmail.com>
2019-04-09 05:19:16 +0300

    Output info about product

--- app/views/Product/view.php
+++ app/views/Product/view.php
@@ -33,9 +33,13 @@
                         <!-- FlexSlider -->
 
                     </div>
+                    <?php
+                        $curr = \ishop\App::$app->getProperty('currency');
+                        $cats = \ishop\App::$app->getProperty('cats');
+                    ?>
                     <div class="col-md-7 single-top-right">
                         <div class="single-para simpleCart_shelfItem">
-                            <h2>Lorem Ipsum</h2>
+                            <h2><?=$product->title;?></h2>
                             <div class="star-on">
                                 <ul class="star-footer">
                                     <li><a href="#"><i> </i></a></li>
@@ -51,8 +55,17 @@
                                 <div class="clearfix"> </div>
                             </div>
 
-                            <h5 class="item_price">$ 95.00</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                            <h5 class="item_price">
+                                <?=$curr['symbol_left'];?> <?= $product->price * $curr['value']; ?> <?=$curr['symbol_right'];?>
+                            </h5>
+                            <?php if ($product->old_price) : ?>
+                                <del>
+                                        <?=$curr['symbol_left'];?>
+                                        <?= $product->old_price * $curr['value']; ?>
+                                        <?=$curr['symbol_right'];?>
+                                </del>
+                            <?php endif; ?>
+                            <p><?=$product->content;?></p>
                             <div class="available">
                                 <ul>
                                     <li>Color
@@ -73,13 +86,25 @@
                                 </ul>
                             </div>
                             <ul class="tag-men">
-                                <li><span>TAG</span>
-                                    <span class="women1">: Women,</span></li>
-                                <li><span>SKU</span>
-                                    <span class="women1">: CK09</span></li>
+                                <li><span>Category</span>
+                                    <span>:
+                                        <a href="category/<?=$cats[$product->category_id]['alias'];?>">
+                                            <?=$cats[$product->category_id]['title'];?>
+                                        </a>
+                                    </span>
+                                </li>
                             </ul>
-                            <a href="#" class="add-cart item_add">ADD TO CART</a>
-
+                            <div class="quantity">
+                                <input type="number" size="4" value="1" name="quantity" min="1" step="1">
+                                <a
+                                        href="cart/add?id=<?=$product->id;?>"
+                                        class="add-cart item_add add-to-cart-link"
+                                        id="productAdd"
+                                        data-id="<?=$product->id;?>"
+                                >
+                                    ADD TO CART
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="clearfix"> </div>
