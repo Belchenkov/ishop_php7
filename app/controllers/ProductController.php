dcf406a058bdecb022bbbce7bf80e30b80ec2d67
Aleksey Belchenkov <u608110@gmail.com>
2019-04-10 06:45:12 +0300

    Recently View products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+
 class ProductController extends AppController
 {
     public function viewAction()
@@ -15,22 +17,30 @@ class ProductController extends AppController
 
         // TODO breadcrumbs
 
-        // TODO chain products
+        // chain products
         $related = \R::getAll(
             "SELECT * FROM related_product
                   JOIN product ON product.id = related_product.related_id
                    WHERE related_product.product_id = ?", [$product->id]
         );
-        // TODO record cookies products
+        // record cookies products
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
 
-        // TODO просмотренные товары
+        // просмотренные товары
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
 
-        // TODO Gallery
+        if ($r_viewed) {
+            $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
+        }
+
+        // Gallery
         $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
 
         // TODO Modificate
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery'));
+        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed'));
     }
 }
