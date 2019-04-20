2e8b14b6c386cf264738c1bc6cffbcc9b7d3ab4f
Aleksey Belchenkov <u608110@gmail.com>
2019-04-20 06:54:51 +0300

    Pagination

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -1,7 +1,10 @@
 <?php
 namespace app\controllers;
 
+use app\models\Breadcrumbs;
 use app\models\Category;
+use ishop\App;
+use ishop\libs\Pagination;
 
 class CategoryController extends AppController
 {
@@ -15,16 +18,28 @@ class CategoryController extends AppController
         }
 
         // Breadcrumbs
-        $breadcrumbs = '';
+        $breadcrumbs = Breadcrumbs::getBreadcrumbs($category->id);
 
         $cat_model = new Category();
         $ids = $cat_model->getIds($category->id);
         $ids = !$ids ? $category->id : $ids . $category->id;
 
-        $products = \R::find('product', "category_id IN ($ids)");
+        // Pagination
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = App::$app->getProperty('pagination');
+        $total = \R::count('product', "category_id IN ($ids)");
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $products = \R::find('product', "category_id IN ($ids) LIMIT $start, $perpage");
 
         $this->setMeta($category->title, $category->description, $category->keywords);
-        $this->set(compact('products', 'breadcrumbs'));
+        $this->set(compact(
+            'products',
+            'breadcrumbs',
+            'pagination',
+            'total'
+        ));
 
     }
 }
--- app/views/Category/view.php
+++ app/views/Category/view.php
@@ -3,8 +3,7 @@
     <div class="container">
         <div class="breadcrumbs-main">
             <ol class="breadcrumb">
-                <li><a href="<?=PATH;?>">Главная</a></li>
-                <li>Категория</li>
+                <?=$breadcrumbs;?>
             </ol>
         </div>
     </div>
@@ -54,6 +53,12 @@
                             </div>
                         <?php endforeach; ?>
                         <div class="clearfix"></div>
+                        <div class="text-center">
+                            <p>(<?=count($products);?> товара(ов) из <?=$total;?>)</p>
+                            <?php if ($pagination->countPages > 1) : ?>
+                                <?=$pagination;?>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 <?php else: ?>
                     <h3>В этой категории товаров пока нет...</h3>
