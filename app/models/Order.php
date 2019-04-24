ac52e40c10ceb02a6c3943075cdebefb29dae983
Aleksey Belchenkov <u608110@gmail.com>
2019-04-24 05:08:18 +0300

    Create Page of Order

---
+++ app/models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\models;
+
+class Order extends AppModel
+{
+    public static function saveOrder($data)
+    {
+
+    }
+
+    public static function saveOrderProduct($order_id)
+    {
+
+    }
+
+    public static function mailOrder($order_id, $user_email)
+    {
+
+    }
+}
