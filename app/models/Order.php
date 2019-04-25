9441ac51d76d23c9ca884752ae70d434b83785af
Aleksey Belchenkov <u608110@gmail.com>
2019-04-25 04:51:42 +0300

    Save Order

--- app/models/Order.php
+++ app/models/Order.php
@@ -4,7 +4,8 @@ namespace app\models;
 
 class Order extends AppModel {
 
-    public static function saveOrder($data){
+    public static function saveOrder($data)
+    {
         $order = \R::dispense('order');
         $order->user_id = $data['user_id'];
         $order->note = $data['note'];
@@ -14,11 +15,20 @@ class Order extends AppModel {
         return $order_id;
     }
 
-    public static function saveOrderProduct($order_id){
+    public static function saveOrderProduct($order_id)
+    {
+        $sql_part = '';
 
+        foreach ($_SESSION['cart'] as $product_id => $product) {
+            $product_id = (int) $product_id;
+            $sql_part .= "($order_id, $product_id, {$product['qty']}, '{$product['title']}', {$product['price']}),";
+        }
+        $sql_part = rtrim($sql_part, ',');
+        \R::exec("INSERT INTO order_product (order_id, product_id, qty, title, price) VALUES $sql_part");
     }
 
-    public static function mailOrder($order_id, $user_email){
+    public static function mailOrder($order_id, $user_email)
+    {
 
     }
 
