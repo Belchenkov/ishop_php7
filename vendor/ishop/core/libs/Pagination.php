08cba8cec39fcef16aa24d357846d1cac247188f
Aleksey Belchenkov <u608110@gmail.com>
2019-05-03 10:02:53 +0300

    List of Orders

--- vendor/ishop/core/libs/Pagination.php
+++ vendor/ishop/core/libs/Pagination.php
@@ -76,15 +76,12 @@ class Pagination{
 
     public function getParams(){
         $url = $_SERVER['REQUEST_URI'];
-        preg_match_all("#filter=[\d,&]#, $url, $matches");
-
-        if (count($matches[0]) > 1) {
+        preg_match_all("#filter=[\d,&]#", $url, $matches);
+        if(count($matches[0]) > 1){
             $url = preg_replace("#filter=[\d,&]+#", "", $url, 1);
         }
-
         $url = explode('?', $url);
         $uri = $url[0] . '?';
-
         if(isset($url[1]) && $url[1] != ''){
             $params = explode('&', $url[1]);
             foreach($params as $param){
