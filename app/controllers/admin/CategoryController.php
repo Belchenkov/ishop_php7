665e57e8adce52381907d174fbd7f8ed0192f471
Aleksey Belchenkov <u608110@gmail.com>
2019-05-08 06:13:32 +0300

    Edit Category

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace app\controllers\admin;
 
 use app\models\AppModel;
 use app\models\Category;
+use ishop\App;
 
 class CategoryController extends AppController
 {
@@ -60,5 +61,38 @@ class CategoryController extends AppController
             redirect();
         }
         $this->setMeta('Новая категория');
+    }
+
+    public function editAction()
+    {
+        if (!empty($_POST)) {
+            $id = $this->getRequestID(false);
+            $category = new Category();
+            $data = $_POST;
+            $category->load($data);
+
+            if (!$category->validate($data)) {
+                $category->getErrors();
+                redirect();
+            }
+
+            if ($category->update('category', $id)) {
+                $alias = AppModel::createAlias('category', 'alias', $data['title'], $id);
+                $category = \R::load('category', $id);
+                $category->alias = $alias;
+                \R::store($category);
+                $_SESSION['success'] = 'Изменения сохранены';
+            }
+
+            redirect();
+        }
+
+        $id = $this->getRequestID();
+        $category = \R::load('category', $id);
+
+        App::$app->setProperty('parent_id', $category->parent_id);
+
+        $this->setMeta("Редактирование категрии {$category->title}");
+        $this->set(compact('category'));
     }
 }
