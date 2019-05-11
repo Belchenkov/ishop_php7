02e91d7d7d7136625f88b6a8226520af18691f49
Aleksey Belchenkov <u608110@gmail.com>
2019-05-11 19:13:52 +0300

    Show All Users

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -3,9 +3,32 @@
 namespace app\controllers\admin;
 
 use app\models\User;
+use ishop\libs\Pagination;
 
 class UserController extends AppController
 {
+    public function indexAction()
+    {
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = \R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $users = \R::findAll('user', "LIMIT $start, $perpage");
+
+        $this->setMeta('Список пользователей');
+        $this->set(compact('users', 'pagination', 'count'));
+    }
+
+    public function editAction()
+    {
+        $user_id = $this->getRequestID();
+        $user = \R::load('user', $user_id);
+
+        $this->setMeta('Редактирование профиля пользователя');
+        $this->set(compact('user'));
+    }
+    
     public function loginAdminAction()
     {
         if (!empty($_POST)) {
