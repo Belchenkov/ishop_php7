ff27e9454d61edfd1c074fd097cc2330da6f1c2b
Aleksey Belchenkov <u608110@gmail.com>
2019-05-23 07:19:39 +0300

    Add New Filter

--- app/controllers/admin/FilterController.php
+++ app/controllers/admin/FilterController.php
@@ -4,6 +4,7 @@
 namespace app\controllers\admin;
 
 
+use app\models\admin\FilterAttr;
 use app\models\admin\FilterGroup;
 
 class FilterController extends AppController
@@ -17,9 +18,36 @@ class FilterController extends AppController
 
     public function attributeAction()
     {
-
+        $attrs = \R::getAssoc("
+            SELECT attribute_value.*, attribute_group.title FROM attribute_value 
+            JOIN attribute_group ON attribute_group.id = attribute_value.attr_group_id");
+        $this->setMeta('Фильтры');
+        $this->set(compact('attrs'));
     }
 
+    public function attributeAddAction()
+    {
+        if (!empty($_POST)) {
+            $attr = new FilterAttr();
+            $data = $_POST;
+            $attr->load($data);
+
+            if (!$attr->validate($data)) {
+                $attr->getErrors();
+                redirect();
+            }
+
+            if ($attr->save('attribute_value', false)) {
+                $_SESSION['success'] = 'Добавлен атрибут';
+                redirect();
+            }
+        }
+
+        $group = \R::findAll('attribute_group');
+        $this->set(compact('group'));
+        $this->setMeta('Новый фильтр');
+    }
+    
     public function groupAddAction()
     {
         if (!empty($_POST)) {
