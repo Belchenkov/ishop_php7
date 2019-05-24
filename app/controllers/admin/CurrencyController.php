652078acc44b4be2e369386bbf27a76d8116c3f1
Aleksey Belchenkov <u608110@gmail.com>
2019-05-24 08:32:55 +0300

    Edit/Delete Currency

--- app/controllers/admin/CurrencyController.php
+++ app/controllers/admin/CurrencyController.php
@@ -34,4 +34,37 @@ class CurrencyController extends AppController
 
         $this->setMeta('Новая валюта');
     }
+
+    public function deleteAction()
+    {
+        $id = $this->getRequestID();
+        $currency = \R::load('currency', $id);
+        \R::trash($currency);
+        $_SESSION['success'] = "Изменения сохранены";
+        redirect();
+    }
+
+    public function editAction()
+    {
+        if (!empty($_POST)){
+            $id = $this->getRequestID(false);
+            $currency = new Currency();
+            $data = $_POST;
+            $currency->load($data);
+            $currency->attributes['base'] = $currency->attributes['base'] ? '1' : '0';
+            if(!$currency->validate($data)){
+                $currency->getErrors();
+                redirect();
+            }
+            if($currency->update('currency', $id)){
+                $_SESSION['success'] = "Изменения сохранены";
+                redirect();
+            }
+        }
+
+        $id = $this->getRequestID();
+        $currency = \R::load('currency', $id);
+        $this->setMeta("Редактирование валюты {$currency->title}");
+        $this->set(compact('currency'));
+    }
 }
